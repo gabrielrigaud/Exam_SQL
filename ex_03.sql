a11d1efe9a7f760798635005509f9da0ec37852e
gabriel <gabriel.rigaud@epitech.eu>
2026-04-21 14:23:49 +0200

    upload ex_01.sql,ex_02.sql,ex_03.sql

---
+++ ex_03.sql
@@ -0,0 +1,5 @@
+/*
+
+SELECT * cette commande selectionne toutes les colonnes de la table car il y'a '(*) .
+
+Alors que avec SELECT colonne on sélectionne une seule colonne qui dans l'exemple s'appel "colonne" .  /*
